7e4db84dcd22af4e29d2b42e5762eae863cfa27d
artemsavenkov <artem13101992>
2022-12-16 23:01:37 +0200

    Add env variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'ololo 777. DB user: ' + process.env.DB_USER;
+    return `<h1>ololo 777. DB user: ${process.env.DB_USER}</h1>`;
   }
 }
