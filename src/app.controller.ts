bcae588992e576447c718ddc35ae0c8a0a093773
artemsavenkov <artem13101992>
2022-12-16 22:59:18 +0200

    Add env variables

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,7 +8,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'ololo 777';
-    // return this.appService.getHello();
+    return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,14 +7,18 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
 
+const dbConnection: string = `mongodb+srv://${
+  process.env.DB_USER || 'admin'
+}:${
+  process.env.DB_PASSWORD || 'admin'
+}@cluster0.okbhktt.mongodb.net/products?retryWrites=true&w=majority`;
+
 @Module({
   imports: [
     ProductsModule,
     ProductsModule,
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.okbhktt.mongodb.net/products?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(dbConnection),
     AuthModule,
     OrderModule,
   ],
